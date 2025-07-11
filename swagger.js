ab703f11a4fbe3fcb187bd1cc2ef252fbdda08d6
mariam khaled <khaldmrym769@gmail.com>
2025-07-11 20:16:26 +0300

    first

--- swagger.js
+++ swagger.js
@@ -1,63 +1,26 @@
-import swaggerAutogen from 'swagger-autogen';
+import swaggerAutogen from "swagger-autogen";
 
 const doc = {
   info: {
-    title: 'Topamine API Documentation',
-    version: '1.0.0',
-    description:
-      'Welcome to the official Topamine API documentation. This document provides a comprehensive overview of all available endpoints for developers to interact with the Topamine platform.',
+    title: "Topamine API",
+    description: "API Documentation for the Topamine platform.",
   },
-  host: 'topamun-backend.vercel.app',
-  schemes: ['https'],
+  host: "topamun-backend.vercel.app", // Deployed URL
+  schemes: ["https"],
+  consumes: ["application/json", "multipart/form-data"],
+  produces: ["application/json"],
   securityDefinitions: {
     bearerAuth: {
-        type: 'http',
-        scheme: 'bearer',
-        bearerFormat: 'JWT'
-    }
+      type: "apiKey",
+      name: "token", // The header name
+      in: "header",
+      description:
+        "Enter your bearer token in the format 'Bearer {token}'",
+    },
   },
-  definitions: {
-    StudentRegistration: {
-        $firstName: "Ahmed",
-        $lastName: "Ali",
-        $email: "student@example.com",
-        $password: "password123",
-        $confirmPassword: "password123",
-        $phone: "1234567890",
-        $province: "Cairo",
-        $role: "Student",
-        $academicStage: "High School"
-    },
-    TeacherRegistration: {
-        $firstName: "Fatima",
-        $lastName: "Zahra",
-        $email: "teacher@example.com",
-        $password: "password123",
-        $confirmPassword: "password123",
-        $phone: "0987654321",
-        $province: "Alexandria",
-        $role: "Teacher",
-        $subject: "Physics",
-        certificate: "file"
-    },
-    Login: {
-        $email: "user@example.com",
-        $password: "password123"
-    },
-    ForgetCode: {
-        $email: "user@example.com",
-    },
-    ResetPassword: {
-        $email: "user@example.com",
-        $forgetCode: "12345",
-        $password: "newPassword123",
-        $confirmPassword: "newPassword123"
-    }
-  }
 };
 
-const outputFile = './src/swagger-output.json';
-const endpointsFiles = ['./src/index.router.js']; 
+const outputFile = "./src/swagger-output.json";
+const endpointsFiles = ["./src/index.router.js"]; // Main router file that includes all other routers
 
-// Generate swagger.json
-swaggerAutogen({openapi: '3.0.0'})(outputFile, endpointsFiles, doc); 
+swaggerAutogen({ openapi: "3.0.0" })(outputFile, endpointsFiles, doc); 
